1caac58d80f24561a3d9800283113107e59a3a2a
LeeNa A.Al Zaben <125594282+Leenaalzaben@users.noreply.github.com>
2023-08-04 18:26:54 +0300

    Update hashtable.py

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,127 +1,140 @@
 from functools import reduce
 from operator import add
-import string
-
 
 class Node:
-    '''
-    A class represent a node in a linked list or other data structure each node has two main componenet the value of the node and the reference to the next node.
-    args: value
-    return : nothing
-    '''
+  '''
+  A class represent a node in a linked list or other data structure each node has two main componenet the value of the node and the reference to the next node.
+  args: value
+  return : nothing
+  '''
+  def __init__(self, value):
+      self.next=None 
+      self.value=value
 
-    def __init__(self, value):
-        self.next = None
-        self.value = value
 
 
 class LinkedList:
+  '''
+  what : A class representing a singly linked list data structure
+  '''
+  def __init__(self):
+    self.head = None
+
+
+  def insert (self, value):
     '''
-    what : A class representing a singly linked list data structure
+    insert a new node with the given value at the begining of     the linked list.
+    args: value
+    output : none
+    
     '''
-
-    def __init__(self):
-        self.head = None
-
-    def insert(self, value):
-        '''
-        insert a new node with the given value at the begining of     the linked list.
-        args: value
-        output : none
-
-        '''
-        new_node = Node(value)
-        new_node.next = self.head
-        self.head = new_node
+    new_node = Node(value)
+    new_node.next = self.head
+    self.head = new_node
 
 
 class HashTable:
+  '''
+  what : data structure that store key-value pairs of data using buckets to increace data accessing efficiency 
+  
+  '''
+  def __init__(self,size=1024):
+    self.__size=size
+    self.__buckets=[None] *size
+    self.keys = []
+    
+  
+  def __hash(self,key):
     '''
-    what : data structure that store key-value pairs of data using buckets to increace data accessing efficiency 
-
+    A method to return the hash code of the given key
+    arg : key
+    output: hash code of the key(index)
     '''
+    # code = 0
+   
+    # for char in key:
+    #   code += ord(str(char)) # * weight
+    # code *= 255
+    # code = code % 1024
+    # return code
+    return reduce(add, [ord(str(char)) for char in key]) * 283 % self.__size
+    return sum([ord(str(char)) for char in key]) * 283 % self.__size
 
-    def __init__(self, size=1024):
-        self.__size = size
-        self.__buckets = [None] * size
-        self.key_list = []
+  
+    
+  def set(self,key,value):
+    '''
+    Set a key-value pair in the bucket, handling collisions as              needed.
+    Arguments:
+    key : The key to be hashed and used as the identifier for the           value.
+    value : the value that is aassociated with the key
+    Returns: None
+    '''
+    index = self.__hash(key)
+    if self.__buckets[index] is None:
+      ll = LinkedList()
+      self.__buckets[index] = ll
+     
+    self.__buckets[index].insert([key,value])
+    self.keys.append(key)
+    
+    
 
-    def __hash(self, key):
-        '''
-        A method to return the hash code of the given key
-        arg : key
-        output: hash code of the key(index)
-        '''
+ 
 
-        return reduce(add, [ord(str(char)) for char in key]) * 283 % self.__size
-        # return sum([ord(str(char)) for char in key]) * 283 % self.__size
+  def get(self,key):
+    '''
+    Retrieve the value with the given key from the hashtable
+    arg : key
+    return : value or None 
+    '''
+    index=self.__hash(key)
+    bucket = self.__buckets[index]
+    if bucket is not None : 
+      curr = bucket.head
+      while curr :
+        if curr.value[0] == key :
+          return curr.value[1]
+        curr = curr.next  
+    return None  
+    
+    
 
-    def set(self, key, value):
-        '''
-        Set a key-value pair in the bucket, handling collisions as              needed.
-        Arguments:
-        key : The key to be hashed and used as the identifier for the           value.
-        value : the value that is aassociated with the key
-        Returns: None
-        '''
-        index = self.__hash(key)
-        if self.__buckets[index] is None:
-            ll = LinkedList()
-            self.__buckets[index] = ll
+  def has(self, key):
+    '''
+    A method to check if the given key exist in the hashtable.
+    arg: key
+    output: boolean
+    ''' 
+    if self.get(key):
+      return True
+    return False  
 
-        self.__buckets[index].insert([key, value])
-        self.key_list.append(key)
+    
 
-    def get(self, key):
-        '''
-        Retrieve the value with the given key from the hashtable
-        arg : key
-        return : value or None 
-        '''
-        index = self.__hash(key)
-        bucket = self.__buckets[index]
-        if bucket is not None:
-            curr = bucket.head
-            while curr:
-                if curr.value[0] == key:
-                    return curr.value[1]
-                curr = curr.next
-        return None
+  def get_keys(self):
+    '''
+    args : none
+    Returns a list of all the  keys present in the Hashtable.
+    '''
+    return self.keys
 
-    def has(self, key):
-        '''
-        A method to check if the given key exist in the hashtable.
-        arg: key
-        output: boolean
-        '''
-        # index=self.__hash(key)
-        # bucket = self.__buckets[index]
-        # if bucket is not None :
-        #   curr = bucket.head
-        #   while curr :
-        #     if curr.value[0] == key :
-        #       return True
-        #     curr = curr.next
-        #   return False
-        if self.get(key):
-            return True
-        return False
 
-    def keys(self):
-        '''
-        args : none
-        Returns a list of all the  keys present in the Hashtable.
-        '''
-        return self.key_list
 
-    def repeated_word(self, s):
-        translator = str.maketrans('', '', string.punctuation)
-        s = s.translate(translator)
-        words = s.split()
-        word_freq = {}
-        for word in words:
-            word = word.lower()
-            if word in word_freq:
-                return word
-            word_freq[word] = 1
-        return None
+def repeated_word(string):
+    valid_chars = "abcdefghijklmnopqrstuvwxyz "
+    words = "".join([i for i in string.lower() if i in valid_chars])
+    hashs = HashTable()
+    for i in words.split():
+        if hashs.has(i):
+            return i
+        hashs.set(i, "i")
+
+    return "no words found"
+
+str1='Once upon a time, there was a brave princess who...'
+str2='It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...'
+str3='It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...'
+print(1,repeated_word(str1))
+print(2,repeated_word(str2))
+print(3,repeated_word(str3))
