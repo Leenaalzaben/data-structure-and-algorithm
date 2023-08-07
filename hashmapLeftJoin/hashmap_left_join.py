f35d0a32fc52edb95444a54ba48967ad14b4335b
LeeNa A.Al Zaben <125594282+Leenaalzaben@users.noreply.github.com>
2023-08-07 17:34:21 +0300

    Update hashmap_left_join.py

--- hashmapLeftJoin/hashmap_left_join.py
+++ hashmapLeftJoin/hashmap_left_join.py
@@ -1,25 +1,52 @@
-def left_join(dict1,dict2):
-    
-    output = []
-    for key in dict1.keys():
-        if key in dict2.keys():
+class HashTable:
+    def __init__(self, size=100):
+        self.size = size
+        self.table = [[] for _ in range(size)]
 
-            output.append([key,dict1[key],dict2[key]])
-        else:
-            output.append([key,dict1[key],None])
+    def _hash(self, key):
+        return sum(ord(c) for c in key) % self.size
 
-    return output
+    def set(self, key, value):
+        index = self._hash(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+        self.table[index].append([key, value])
 
-if __name__ == "__main__":
-    dict1 = {'fond':'enamored',
-    'wrath':'anger',
-    'diligent':'employed',
-    'outfit':'garb',
-    'guide':'usher'}
+    def get(self, key):
+        index = self._hash(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                return pair[1]
+        return None
 
-    dict2 = {'fond':'averse',
-    'wrath':'delight',
-    'diligent':'idle',
-    'guide':'follow',
-    'flow':'jam'}
-    print("The result ",left_join(dict1,dict2))
+    def has(self, key):
+        index = self._hash(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                return True
+        return False
+
+    def keys(self):
+        keys = []
+        for bucket in self.table:
+            for pair in bucket:
+                keys.append(pair[0])
+        return list(set(keys))  
+ 
+ 
+
+def hashmap_left_join(T1: HashTable, T2: HashTable):
+        """
+        Function to left join two hashmaps.
+
+        Returns:
+            Hashtable: Hashtable containing the left join result with keys from table1 and values from both tables.
+        """
+        htb = HashTable()
+        for key in T1.keys():
+            value1 = T1.get(key)
+            value2 = T2.get(key)
+            htb.set(key, [value1, value2])
+        return htb
